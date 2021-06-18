63821bb6208168dcf5af39b7356f7c41c128425b
Ujjwal Gaba <200457461@student.georgianc.on.ca>
2021-06-18 14:03:20 -0400

    Add project files.

---
+++ OnlineStudyApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineStudyApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
