aa205edee3f6ded94abb131ac69d6346e8a9958b
Ujjwal Gaba <200457461@student.georgianc.on.ca>
2021-06-21 17:57:11 -0400

    Created the views with scaffolding.

---
+++ OnlineStudyApplication/Controllers/StudiesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineStudyApplication.Data;
+using OnlineStudyApplication.Models;
+
+namespace OnlineStudyApplication.Controllers
+{
+    public class StudiesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Studies
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Studies.Include(s => s.Course);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Studies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var study = await _context.Studies
+                .Include(s => s.Course)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (study == null)
+            {
+                return NotFound();
+            }
+
+            return View(study);
+        }
+
+        // GET: Studies/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "CourseName");
+            return View();
+        }
+
+        // POST: Studies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ChapterName,ChapterDescription,FileUpload,CourseId")] Study study)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(study);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "CourseName", study.CourseId);
+            return View(study);
+        }
+
+        // GET: Studies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var study = await _context.Studies.FindAsync(id);
+            if (study == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "CourseName", study.CourseId);
+            return View(study);
+        }
+
+        // POST: Studies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ChapterName,ChapterDescription,FileUpload,CourseId")] Study study)
+        {
+            if (id != study.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(study);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudyExists(study.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "CourseName", study.CourseId);
+            return View(study);
+        }
+
+        // GET: Studies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var study = await _context.Studies
+                .Include(s => s.Course)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (study == null)
+            {
+                return NotFound();
+            }
+
+            return View(study);
+        }
+
+        // POST: Studies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var study = await _context.Studies.FindAsync(id);
+            _context.Studies.Remove(study);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudyExists(int id)
+        {
+            return _context.Studies.Any(e => e.Id == id);
+        }
+    }
+}
